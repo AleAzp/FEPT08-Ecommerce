4c1f313beda2c9c2f4d37d1d9f08de9e66355372
AleAzp <aleazpurua@gmail.com>
2024-12-03 10:44:07 +0100

    first

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">{{title}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data (){
+    return{
+      title: "hola",
+    }
+  }
+  
+}
+</script>
+
+<style>
+  
+</style>
